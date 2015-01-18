6c7ce291cd74f4fa6fcdf8278bad69b885555108
schmalls <jthompson@miratechcorp.com>
2015-01-18 11:57:37 -0600

    Bump version to 0.3.4

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.3")]
-[assembly: AssemblyFileVersion("0.3.3")]
+[assembly: AssemblyVersion("0.3.4")]
+[assembly: AssemblyFileVersion("0.3.4")]
